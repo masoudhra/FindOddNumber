e5246ef29e65dcfafaa7984dc2d322085838cbb7
Masoud Ramezani <masoudhra@gmail.com>
2023-07-27 16:59:44 +1200

    find odd number between num1 and num2

---
+++ tamrin/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindOddNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the second number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            
+            if (num1 > num2)
+            {
+                int temp = num1;
+                num1 = num2;
+                num2 = temp;
+            }
+
+            Console.WriteLine("Odd numbers between " + num1 + " and " + num2 + " are:");
+
+            for (int i = num1; i <= num2; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
